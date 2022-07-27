5e05bad37e668b784139a227651f97eeb6f1ff62
yuvika02 <trikhayuvika20@gmail.com>
2022-07-27 21:59:34 +0530

    my login page
    login page

---
+++ app/src/main/java/com/example/yuvikafirstproject/ForgotPasswrodActivity.kt
@@ -0,0 +1,11 @@
+package com.example.yuvikafirstproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForgotPasswrodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
--- app/src/main/java/com/example/yuvikafirstproject/MainActivity.kt
+++ app/src/main/java/com/example/yuvikafirstproject/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.yuvikafirstproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import android.view.View
+import android.view.inputmethod.InputBinding
 import android.widget.*
 import androidx.core.widget.doOnTextChanged
-import android.widget.Toast.makeText as toastMakeText
-
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:BindingMainActivity
     lateinit var etName: EditText
     lateinit var etPassword: EditText
     lateinit var tvForgotPassword: TextView
@@ -21,10 +23,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        etName = findViewById(R.id.etName)
-        etPassword = findViewById(R.id.etPassword)
-        tvForgotPassword = findViewById(R.id.tvForgotPassword)
-        btnLogin = findViewById(R.id.btnLogin)
+//        etName = findViewById(R.id.etName)
+//        etPassword = findViewById(R.id.etPassword)
+//        etOtherName = findViewById(R.id.etOtherName)
+//        tvForgotPassword = findViewById(R.id.tvForgotPassword)
+//        btnLogin = findViewById(R.id.btnLogin)
+        rbHe = findViewById(R.id.rbHe)
+        rbShe = findViewById(R.id.rbShe)
+        rbOther = findViewById(R.id.rbOthers)
+        rgGender = findViewById(R.id.rgGender)
         etPassword.doOnTextChanged { text, _, _, _ ->
             if ((text?.length ?: 0) < 6) {
                 etPassword.error = resources.getString(R.string.please_enter_password)
@@ -34,13 +41,16 @@ class MainActivity : AppCompatActivity() {
         }
         rbHe = findViewById(R.id.rbHe)
         rbShe = findViewById(R.id.rbShe)
-        rbOther = findViewById(R.id.rbOther)
+        rbOther = findViewById(R.id.rbOthers)
         rgGender = findViewById(R.id.rgGender)
-        rgGender.setOnCheckedChangeListener { radioGroup, id ->
+        rgGender.setOnCheckedChangeListener() { RadioGroup, id ->
             when (id) {
-                R.id.rbOther -> {
-                    toastMakeText(resources.getString(R.string.others), this, Toast.LENGTH_LONG)
-
+                R.id.rbOthers -> {
+                    Toast.makeText(
+                        this,
+                        resources.getString(R.string.others),
+                        Toast.LENGTH_LONG
+                    ).show()
                     etOtherName.visibility = View.VISIBLE
                 }
                 else -> {
@@ -48,27 +58,45 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
         rbOther.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 etOtherName.visibility = View.VISIBLE
             } else {
                 etOtherName.visibility = View.INVISIBLE
             }
+            btnLogin.setOnClickListener {
+                etName.text.clear()
+                System.out.println("Clicked")
+                var name = etName.text.toString()
+                var password = etPassword.text.toString()
+                System.out.println("name $name")
+                if(name.isNullOrEmpty()){
+                    etName.error = resources.getString(R.string.please_enter_name)
+                    etName.requestFocus()
+                }else if(password.isNullOrEmpty()){
+                    etPassword.error = resources.getString(R.string.please_enter_password)
+                    etPassword.requestFocus()
+                }else if(password.length<6){
+                etPassword.error = resources.getString(R.string.please_enter_password)
+                etPassword.requestFocus()
+            }
+                else{
+                Toast.makeText(this, resources.getString(R.string.login_successfully), Toast.LENGTH_LONG).show()
+                var intent = Intent(this, ForgotPasswrodActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            }
         }
 
-        btnLogin.setOnClickListener {
-            etName.text.clear()
-
-            System.out.println("Clicked")
-            var name = etName.text.toString()
-            var password = etPassword.text.toString()
-
-        }
 
 
     }
-}
+    }
+
+
+
+
 
 
 
